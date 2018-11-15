4acabc8298953acea1afbadd480f035753eae7c2
Joseph-Kung <joseph.kung@hotmail.com>
2018-11-15 10:46:47 -0800

    Create sign up form for user and auth token

--- music_app/config/routes.rb
+++ music_app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create, :update, :delete]
-  
+  resources :users, only: [:new, :create, :update, :delete, :show]
+
   resource :session, only: [:new, :create, :delete]
 end
